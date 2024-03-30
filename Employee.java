3c2a64c097716ededbb6859f79b99ecfc0162eb7
meghana1617 <113576947+meghana1617@users.noreply.github.com>
2024-03-30 19:30:21 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+
+class Employee implements Comparable<Employee> {
+    private int id;
+    private String name;
+    private String designation;
+    private double salary;
+
+    
+    public Employee(int id, String name, String designation, double salary) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+  
+    @Override
+    public int compareTo(Employee other) {
+        return this.name.compareTo(other.getName());
+    }
+
+    
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Designation: " + designation + ", Salary: " + salary;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        LinkedList<Employee> employees = new LinkedList<>();
+
+        
+        employees.add(new Employee(101, "John", "Manager", 50000));
+        employees.add(new Employee(102, "Alice", "Developer", 60000));
+        employees.add(new Employee(103, "Bob", "Analyst", 55000));
+
+        
+        Collections.sort(employees, Comparator.comparing(Employee::getName));
+
+        
+        System.out.println("Sorted by name:");
+        for (Employee emp : employees) {
+            System.out.println(emp);
+        }
+
+        
+        Collections.sort(employees, Comparator.comparingDouble(Employee::getSalary));
+
+        
+        System.out.println("\nSorted by salary:");
+        for (Employee emp : employees) {
+            System.out.println(emp);
+        }
+    }
+}
